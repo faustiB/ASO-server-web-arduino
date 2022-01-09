6b8ea4f06e9a876f9170a8c446be93509f464e7f
andresrpo6397 <andresrpo6397@gmail.com>
2022-01-09 22:01:21 +0000

    se añaden los mp3 en una carpeta y se modifica playlist para su correcta lectura

--- playlist.sh
+++ playlist.sh
@@ -8,7 +8,7 @@ echo -e "<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN"
 	</head>
 <body>
 	<h3>Your Playlist:</h3>
-	$(sudo ls /home/pi/Music | sudo awk 'BEGIN{ print"<table>" }{ print"<tr><td>"$0"</td></tr>" } END { print"</table>"}')
+	$(sudo ls /var/www/cgi-bin/musica | sudo awk 'BEGIN{ print"<table>" }{ print"<tr><td>"$0"</td></tr>" } END { print"</table>"}')
 	<br>
 	<form action='music.sh'>
 		<input type='submit' value='Music player'>
@@ -16,4 +16,3 @@ echo -e "<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN"
 </body>
 </html>
 "
-$(sudo logger -i Playlist is being shown )
